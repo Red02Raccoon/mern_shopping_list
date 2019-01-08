218744c1fe52a545128b4fde7ebe779b6e377453
Nastya Chervona <nastya02chervona@gmail.com>
2019-01-08 21:56:55 +0200

    feat: add navbar

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+	Collapse,
+	Container,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink } from 'reactstrap';
+
+export default class AppNavbar extends React.Component {
+	state = {
+    isOpen: false
+  };
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+	}
+	
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm">
+					<Container>
+						<NavbarBrand href="/">Shopping list</NavbarBrand>
+						<NavbarToggler onClick={this.toggle} />
+						<Collapse isOpen={this.state.isOpen} navbar>
+							<Nav className="ml-auto" navbar>
+								<NavItem>
+									<NavLink href="https://github.com/Red02Raccoon">Github</NavLink>
+								</NavItem>
+							</Nav>
+						</Collapse>
+					</Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
