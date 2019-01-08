1520f272917f9d203e5c4e05249452d6e5f7bad7
Nastya Chervona <nastya02chervona@gmail.com>
2019-01-08 21:11:34 +0200

    feat: add server.js, db, routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items')
 const app = express();
 
 // BodyParesr - middleware.
@@ -15,6 +16,9 @@ mongoose
 	.connect(db)
 	.then(() => console.log('connected'))
 	.catch(err => console.log('errors'));
+
+// Use routes.
+app.use('/api/items', items)
 
 const port = process.env.PORT || 5000;
 
